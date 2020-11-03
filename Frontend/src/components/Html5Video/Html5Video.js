98b2a7a37a8a69882be87a19a14b269e5bd9f68b
Aayush Kumar <ayushk780@gmail.com>
2020-11-03 19:08:49 +0530

    Added video url and anamoly state
    Signed-off-by: Aayush Kumar <ayushk780@gmail.com>

--- Frontend/src/components/Html5Video/Html5Video.js
+++ Frontend/src/components/Html5Video/Html5Video.js
@@ -6,26 +6,24 @@ import "./Html5.css";
 export const VideoJs = (color) => {
   console.log(color);
   return (
-    <URLContext.Consumer>{(context) => {
-      const { setPythonURL } = context;
-      const formSubmit = (a) => {
-        a.preventDefault();
-        setPythonURL(this.state.value);
-      };
-      return (
-      <div className="player-wrapper">
-        <ReactPlayer
-          className="react-player"
-          url={test}
-          width="100%"
-          height="100%"
-          style={{ boxShadow: ` 0 0 45px ${color.color}` }}
-          controls={false}
-          playing={true}
-          muted={true}
-        />
-      </div>
-      )}}
+    <URLContext.Consumer>
+      {(context) => {
+        const { videoURL } = context;
+        return (
+          <div className="player-wrapper">
+            <ReactPlayer
+              className="react-player"
+              url={videoURL}
+              width="100%"
+              height="100%"
+              style={{ boxShadow: ` 0 0 45px ${color.color}` }}
+              controls={true}
+              playing={true}
+              muted={true}
+            />
+          </div>
+        );
+      }}
     </URLContext.Consumer>
   );
 };
