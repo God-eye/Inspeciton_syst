782d5ea861af80973efabdd7053829380e1b2aa6
Aditya-Sihmar <adityasihmar15@gmail.com>
2020-11-08 02:22:12 +0530

    Removed an unnecessary class

--- model_v2.py
+++ model_v2.py
@@ -38,152 +38,152 @@ class Config():
     self.test_size = test_size
     self.tst_seq = tst_seq
 
-##################### Class Preprocessing Functions ###########################################
-class Functions(Config):
-  def __init__(self):
-    Config.__init__(self, test_path, model_path,result_pth)
+# ##################### Class Preprocessing Functions ###########################################
+# class Functions(Config):
+#   def __init__(self):
+#     Config.__init__(self, test_path, model_path,result_pth)
 
-    # load buffer :- frm_cnt : stores the no of frames already loaded of the current video (None represents end of current video)
-    #                indx : stores the indx of the video which is being processed / being loaded
-    #                total : stores the amount of video loaded.                     
-    self.load_buffer = {'frm_cnt': None, 'indx':0, 'total':0}
+#     # load buffer :- frm_cnt : stores the no of frames already loaded of the current video (None represents end of current video)
+#     #                indx : stores the indx of the video which is being processed / being loaded
+#     #                total : stores the amount of video loaded.                     
+#     self.load_buffer = {'frm_cnt': None, 'indx':0, 'total':0}
 
-  def load_batch(self):
-    '''
-    DOCTYPE : This function will load the training videos in a batch of size defined in class Config.
-    Input : None
-    output : Batch of augmentd and processed clips, Total no of videos loaded
-    '''
-    clips = []
-    a = 0
-    q = 0
-    for dir in tqdm(os.walk(self.train_path)):
-      # os.walk() returns an array and the first element of the array represents the subdirectories in our main directory and we want to load the files in the subdirectories.
-      # So we skip the first iteration.
-      a += 1
-      if a == 1:
-        continue
+#   def load_batch(self):
+#     '''
+#     DOCTYPE : This function will load the training videos in a batch of size defined in class Config.
+#     Input : None
+#     output : Batch of augmentd and processed clips, Total no of videos loaded
+#     '''
+#     clips = []
+#     a = 0
+#     q = 0
+#     for dir in tqdm(os.walk(self.train_path)):
+#       # os.walk() returns an array and the first element of the array represents the subdirectories in our main directory and we want to load the files in the subdirectories.
+#       # So we skip the first iteration.
+#       a += 1
+#       if a == 1:
+#         continue
 
-      try:
-        # If the frame count is None or zero then all the frames of that video are loaded and increment video index.
-        if not self.load_buffer['frm_cnt']:
-          self.load_buffer['indx'] += 1
-          self.load_buffer['total'] += 1
+#       try:
+#         # If the frame count is None or zero then all the frames of that video are loaded and increment video index.
+#         if not self.load_buffer['frm_cnt']:
+#           self.load_buffer['indx'] += 1
+#           self.load_buffer['total'] += 1
 
-        # Produced clips according to the load buffer indx. 
-        pth = os.path.join(dir[0], sorted(dir[2])[self.load_buffer['indx']])
-        clips.append(self.load_frames(pth))
+#         # Produced clips according to the load buffer indx. 
+#         pth = os.path.join(dir[0], sorted(dir[2])[self.load_buffer['indx']])
+#         clips.append(self.load_frames(pth))
 
-      except Exception as e:
-        print(e)
-        # The training directory contains two folders so this step will start loading the videos from next directory.
-        self.load_buffer['indx'] = 0
-        continue
+#       except Exception as e:
+#         print(e)
+#         # The training directory contains two folders so this step will start loading the videos from next directory.
+#         self.load_buffer['indx'] = 0
+#         continue
 
-      break
-    return clips, self.load_buffer['total']
+#       break
+#     return clips, self.load_buffer['total']
 
-  def load_frames(self, pth, agmt = True):
-    '''
-    DOCTYPE : This function will load a set of frame sequences from a given video.
-    Input = pth - path of the video, agmt - True (Will apply augmentation) / False (will not apply augmentation)
-    output = numpy array of frame sequences
-    '''
-    video = cv2.VideoCapture(pth)
-    print('\n starting video no : ',self.load_buffer['total'])
-    frames = []
-    cnt = 0
+#   def load_frames(self, pth, agmt = True):
+#     '''
+#     DOCTYPE : This function will load a set of frame sequences from a given video.
+#     Input = pth - path of the video, agmt - True (Will apply augmentation) / False (will not apply augmentation)
+#     output = numpy array of frame sequences
+#     '''
+#     video = cv2.VideoCapture(pth)
+#     print('\n starting video no : ',self.load_buffer['total'])
+#     frames = []
+#     cnt = 0
 
-    while video.isOpened:
-      ret, frame = video.read()
-      cnt += 1
+#     while video.isOpened:
+#       ret, frame = video.read()
+#       cnt += 1
 
-      # If there is any error in loading the next frame. Might be because of ending of the video.
+#       # If there is any error in loading the next frame. Might be because of ending of the video.
       
-      if not ret:
-        print('\nTotal frames read', cnt)
-        self.load_buffer['frm_cnt'] = None
-        print("\nvideo finished.")
-        break
+#       if not ret:
+#         print('\nTotal frames read', cnt)
+#         self.load_buffer['frm_cnt'] = None
+#         print("\nvideo finished.")
+#         break
 
-      # If frm_cnt exists then the previous video was not loaded completely and it will continue the previous sequence.
-      if self.load_buffer['frm_cnt']:
-        if self.load_buffer['frm_cnt'] <= cnt:
-          img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-          frame = cv2.resize(img/256, self.img_size)
-        else:
-          continue
+#       # If frm_cnt exists then the previous video was not loaded completely and it will continue the previous sequence.
+#       if self.load_buffer['frm_cnt']:
+#         if self.load_buffer['frm_cnt'] <= cnt:
+#           img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#           frame = cv2.resize(img/256, self.img_size)
+#         else:
+#           continue
 
-      # If frm_cnt is None then it will start loading the videos from 1st frame.
-      else:
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.resize(img/256, self.img_size)
-      #print('frame shape = ', frame.shape)
-      frames.append(frame.reshape([self.img_size[0], self.img_size[1], 1]))
+#       # If frm_cnt is None then it will start loading the videos from 1st frame.
+#       else:
+#         img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#         frame = cv2.resize(img/256, self.img_size)
+#       #print('frame shape = ', frame.shape)
+#       frames.append(frame.reshape([self.img_size[0], self.img_size[1], 1]))
 
-      # Specifies the maximum no of frames to be loaded
-      if len(frames) >= self.mx_frm:
-        break
+#       # Specifies the maximum no of frames to be loaded
+#       if len(frames) >= self.mx_frm:
+#         break
 
-    # update the frm_cnt variable according to whether the video is completed or not.
-    if ret:
-      self.load_buffer['frm_cnt'] = cnt
-    else:
-      self.load_buffer['frm_cnt'] = None
+#     # update the frm_cnt variable according to whether the video is completed or not.
+#     if ret:
+#       self.load_buffer['frm_cnt'] = cnt
+#     else:
+#       self.load_buffer['frm_cnt'] = None
     
-    video.release()
+#     video.release()
 
-    # If the no of frames loaded are less than the the of frames specified in a sequence then it will dump that sequence.
-    if len(frames) < self.frm_cnt:
-      print('video has insufficiant frames')
-      self.load_buffer['frm_cnt'] = None
-      raise
+#     # If the no of frames loaded are less than the the of frames specified in a sequence then it will dump that sequence.
+#     if len(frames) < self.frm_cnt:
+#       print('video has insufficiant frames')
+#       self.load_buffer['frm_cnt'] = None
+#       raise
 
-    # Perform Augmentation
-    if agmt:
-      frames = self.augment(frames)
+#     # Perform Augmentation
+#     if agmt:
+#       frames = self.augment(frames)
     
-    return np.array(frames)
+#     return np.array(frames)
 
-  def augment(self, frames):
-    '''
-    DOCTYPE : This function will Augment the frames according to the time series strides specified in the Config class.
-    Input : Sequence of frames.
-    Ouput : Augmented Sequence of frames.
-    '''
-    agmted = np.zeros((self.frm_cnt, self.img_size[0], self.img_size[1], 1))
-    clips = []
+#   def augment(self, frames):
+#     '''
+#     DOCTYPE : This function will Augment the frames according to the time series strides specified in the Config class.
+#     Input : Sequence of frames.
+#     Ouput : Augmented Sequence of frames.
+#     '''
+#     agmted = np.zeros((self.frm_cnt, self.img_size[0], self.img_size[1], 1))
+#     clips = []
 
-    try:
-      for strd in self.stride:
-        for s in range(0, len(frames), strd):
-          if len(frames[s:s+self.frm_cnt]) == 10:
-            agmted[:,:,:,:] = frames[s:s+self.frm_cnt]
-            clips. append(agmted)
-    except:
-      print('Error occured in augment')
+#     try:
+#       for strd in self.stride:
+#         for s in range(0, len(frames), strd):
+#           if len(frames[s:s+self.frm_cnt]) == 10:
+#             agmted[:,:,:,:] = frames[s:s+self.frm_cnt]
+#             clips. append(agmted)
+#     except:
+#       print('Error occured in augment')
 
-    no = len(clips) % 8
-    print("clips dropped ",no)
-    clips = clips[:len(clips)-no]
-    return clips
+#     no = len(clips) % 8
+#     print("clips dropped ",no)
+#     clips = clips[:len(clips)-no]
+#     return clips
 
-  def load_single_test(self):
-    test = np.zeros((self.test_size, self.img_size[0], self.img_size[1], 1))
-    for dir in os.listdir(self.test_path):
-      path = os.path.join(self.test_path, dir)
-      frames = self.load_frames(path, agmt = False)
+#   def load_single_test(self):
+#     test = np.zeros((self.test_size, self.img_size[0], self.img_size[1], 1))
+#     for dir in os.listdir(self.test_path):
+#       path = os.path.join(self.test_path, dir)
+#       frames = self.load_frames(path, agmt = False)
 
-    test = frames[0:self.test_size]
-    del frames
-    return test
+#     test = frames[0:self.test_size]
+#     del frames
+#     return test
 
 
 ####################### Model Architecture ##################################
 
-class Model(Functions):
+class Model():
   def __init__(self):
-    Functions.__init__(self)
+    # Functions.__init__(self)
     self.output1 = None
     self.output = None
 
@@ -377,7 +377,7 @@ if __name__ == '__main__':
   result_pth = 'Frontend/anamoly.txt'
   test_path = 'Test'
   cnfg = Config(test_path, model_path,result_pth, tst_seq = 300)
-  fncn = Functions()
+  # fncn = Functions()
   mdl = Model()
   img_dim = (128, 128)
   model = mdl.anom()
